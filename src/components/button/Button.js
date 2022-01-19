d6f041a3f01e870d3146563d11c192ee9e2bf9eb
Asem <maratb3k@gmail.com>
2022-01-20 01:33:12 +0600

    save button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,14 +1,22 @@
 import './Button.css';
+import {useState} from 'react';
 
 function Button() {
+    const [isEditMode, setIsEditMode] = useState(false); 
+
     return (
         <div className="app-button">
-            <button>Update</button>
-            <button>Delete</button>
-            {/* <button onClick={() => deleteItem(items.id)} */}
+            {(!isEditMode) ? 
+                <div className="app-button">
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+            :
+                <button>Save</button>
+            }
+            
         </div>
     );
 }
 
 export default Button;
-
